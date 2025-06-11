7bbdee47d106f350ff6bcc057bdd6498c609ac6a
Yann Kistenmacker <yannkistenmacker@gmail.com>
2025-06-11 16:49:01 -0300

    creating repository.go

---
+++ internal/domain/repository.go
@@ -0,0 +1,15 @@
+package domain
+
+type AccountRepository interface {
+	Save(account *Account) error
+	FindByAPIKey(apiKey string) (*Account, error)
+	FindByID(id string) (*Account, error)
+	UpdateBalance(account *Account) error
+}
+
+type InvoiceRepository interface {
+	Save(invoice *Invoice) error
+	FindByID(id string) (*Invoice, error)
+	FindByAccountID(accountID string) ([]*Invoice, error)
+	UpdateStatus(invoice *Invoice) error
+}
